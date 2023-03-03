ded2683ab5182adf9cbd480b4a396d672ed0cc65
rlfry <rfry33@gmail.com>
2023-03-02 23:17:44 -0600

    Implement workaround for discord-js issue

--- index.js
+++ index.js
@@ -72,5 +72,19 @@ else {
         }
         handleCommand()
     })
+    client.player.on('connectionCreate', (queue) => {
+        queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+            const oldNetworking = Reflect.get(oldState, 'networking');
+            const newNetworking = Reflect.get(newState, 'networking');
+
+            const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+              const newUdp = Reflect.get(newNetworkState, 'udp');
+              clearInterval(newUdp?.keepAliveInterval);
+            }
+
+            oldNetworking?.off('stateChange', networkStateChangeHandler);
+            newNetworking?.on('stateChange', networkStateChangeHandler);
+          });
+    })
     client.login(TOKEN)
 }
